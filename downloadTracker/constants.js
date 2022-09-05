6e17062b5fab6a373826f3c18d5bb2113c2b156d
Cloud <44526554+Cloud7050@users.noreply.github.com>
2022-09-05 12:39:58 +0800

    feat(downloadTracker): Change highlighting style

--- downloadTracker/constants.js
+++ downloadTracker/constants.js
@@ -5,3 +5,5 @@ export const REGEX_SINGLE_DOWNLOAD = /.+\/files\/(?<id>\d+)\/download\?download_
 export const URL_MULTI_DOWNLOAD = "https://*/api/v1/courses/*/content_exports";
 
 export const REGEX_COURSE_ID = /.+\/courses\/(?<id>\d+)\/files.*/u;
+
+export const ORANGE = "#FFAA00";
--- downloadTracker/marker.js
+++ downloadTracker/marker.js
@@ -2,6 +2,9 @@
 //FIXME wait for files to load into DOM, also have a timeout
 //TODO fire marking again after saved download
 (async () => {
+	const { ORANGE } = await import(
+		chrome.runtime.getURL("./downloadTracker/constants.js")
+	);
 	const {
 		extractCourseId,
 		extractFileId
@@ -32,12 +35,17 @@
 		let anchor = row.querySelector("a.ef-name-col__link");
 		if (anchor === null) continue;
 
+		let nameHolder = row.querySelector("span.ef-name-col__text");
+		if (nameHolder === null) return;
+
+		let createdTime = row.querySelector("div.ef-date-created-col time");
+		if (createdTime === null) return;
+
 		let modifiedTime = row.querySelector("div.ef-date-modified-col time");
 		if (modifiedTime === null) continue;
 		let modifiedDate = new Date(
 			modifiedTime.getAttribute("datetime")
 		);
-		d("Modified "+modifiedDate)
 
 		let link = anchor.href;
 		let fileId = extractFileId(link);
@@ -51,42 +59,27 @@
 		let trackedTimestamp = trackedDownloads[fileId] ?? null;
 		if (trackedTimestamp !== null) {
 			let trackedDate = new Date(trackedTimestamp);
-			d("    Tracked "+trackedDate)
 			isNew = false;
 			isModified = modifiedDate > trackedDate;
 		}
 
-		let nameHolder = row.querySelector("span.ef-name-col__text");
-		if (nameHolder !== null) {
-			let nameHolderStyle = nameHolder.style;
-			if (isNew || isModified) {
-				nameHolderStyle.color = "#FFAA00";
-				if (isNew) nameHolderStyle["font-weight"] = "bolder";
-			} else {
-				nameHolderStyle.color = null;
-				nameHolderStyle["font-weight"] = null;
-			}
-		}
-
-		let createdTime = row.querySelector("div.ef-date-created-col time");
-		if (createdTime !== null) {
-			let createdTimeStyle = createdTime.style;
-			if (isNew) {
-				createdTimeStyle.color = "#FFAA00";
-				createdTimeStyle["font-weight"] = "bolder";
-			} else {
-				createdTimeStyle.color = null;
-				createdTimeStyle["font-weight"] = null;
-			}
-		}
-
-		let modifiedTimeStyle = modifiedTime.style;
 		if (isNew || isModified) {
-			modifiedTimeStyle.color = "#FFAA00";
-			if (isNew) modifiedTimeStyle["font-weight"] = "bolder";
+			nameHolder.style.color = ORANGE;
+
+			if (isNew) {
+				createdTime.style.color = ORANGE;
+
+				nameHolder.style["font-weight"] = "bolder";
+				createdTime.style["font-weight"] = "bolder";
+			} else modifiedTime.style.color = ORANGE;
 		} else {
-			modifiedTimeStyle.color = null;
-			modifiedTimeStyle["font-weight"] = null;
+			nameHolder.style.color = null;
+			nameHolder.style["font-weight"] = null;
+
+			createdTime.style.color = null;
+			createdTime.style["font-weight"] = null;
+
+			modifiedTime.style.color = null;
 		}
 	}
 })();
